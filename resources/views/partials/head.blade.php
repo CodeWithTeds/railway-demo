f8fb817be2bd5c33afc325a4b6240b2889d493f3
Tead <teadmarvin@gmail.com>
2025-09-25 03:25:16 +0800

    hala

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,5 +11,6 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://cdn.tailwindcss.com"></script>
 <script src="https://unpkg.com/htmx.org@1.9.12"></script>
 @fluxAppearance
