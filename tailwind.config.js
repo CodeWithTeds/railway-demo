a10b338b29d957d08c4e8f4ddead8521d3f90d73
Tead <teadmarvin@gmail.com>
2025-09-25 03:56:47 +0800

    last na promise lala

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,20 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./vendor/livewire/flux/**/*.blade.php",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+        success: '#10b981',
+        danger: '#ef4444',
+        warning: '#f59e0b',
+        info: '#3b82f6'
+      },
+    },
   },
   plugins: [],
 }
