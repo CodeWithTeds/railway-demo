7c8b12082cdb86861b6c235077ffdbffe7272496
Tead <teadmarvin@gmail.com>
2025-09-25 04:00:02 +0800

    soko nako lala

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,11 @@ module.exports = {
         success: '#10b981',
         danger: '#ef4444',
         warning: '#f59e0b',
-        info: '#3b82f6'
+        info: '#3b82f6',
+        accent: {
+          DEFAULT: '#171717',
+          foreground: '#ffffff'
+        }
       },
     },
   },
